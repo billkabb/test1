45f72e46b9d769da2b0fe3f693b1a882bc00a4ba
billkabb <billkabb@gmail.com>
2022-12-17 03:19:32 +0200

    initials v0.1

---
+++ InitialsApp.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Initials
+ */
+public class InitialsApp {
+
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Τα αρχικά μου \"ΒΚ\" εκτυπωμένα με αστεράκια.\n");
+        System.out.println("*****" + "\t\t*    *");
+        System.out.println("*     *" + "\t\t*   *");
+        System.out.println("*     *" + "\t\t*  *");
+        System.out.println("*****" + "\t\t**");
+        System.out.println("*     *" + "\t\t*  *");
+        System.out.println("*     *" + "\t\t*   *");
+        System.out.println("*****" + "\t\t*    *");
+    }
+}
